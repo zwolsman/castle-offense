15b68c25606327b823ff057cbd9421fdbac46f3f
Goos van den Bekerom <g.vandenbekerom@student.fontys.nl>
2016-03-15 15:04:41 +0100

    Initial commit

---
+++ desktop/src/com/s31b/castleoffense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.s31b.castleoffense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.s31b.castleoffense.CastleOffense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CastleOffense(), config);
+	}
+}
