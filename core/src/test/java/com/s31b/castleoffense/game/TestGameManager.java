214d0b09c571652a44adb63e485d8ae89a6c77e3
Bergh,Dennis D.L.G. van den <dennis.vandenbergh@student.fontys.nl>
2016-04-11 16:05:09 +0200

    Alles Unit Test passed. Only pass if: Not buying entity's. No Texture/SpriteBatches in Constructors. Globals turn SpriteBatch off.

--- core/src/test/java/com/s31b/castleoffense/game/TestGameManager.java
+++ core/src/test/java/com/s31b/castleoffense/game/TestGameManager.java
@@ -5,7 +5,9 @@
  */
 package test.java.com.s31b.castleoffense.game;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.s31b.castleoffense.game.*;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -15,19 +17,25 @@ import org.junit.Before;
  * @author Dennis
  */
 public class TestGameManager {
-    GameManager gm;
-    
-    @Before
-    public void setUp() {
-        gm = new GameManager();
-    }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
     public void TestStartingGame() {
-        //CoGame cg = gm.createGame();
-        //assertEquals(cg, gm.getAllGames().get(0));
+        /*int expected_games = GameManager.getInstance().getAllGames().size() + 1;
+        CoGame cg = GameManager.getInstance().createGame();
+        assertEquals(expected_games, GameManager.getInstance().getAllGames().size());
+        assertEquals(cg, GameManager.getInstance().getAllGames().get(1));*/
+    }
+    
+    @Test
+    public void TestGetters(){
+        assertEquals(2, GameManager.getInstance().getPlayerRowAmount());
+        assertEquals(1000, GameManager.getInstance().getGoldPerWave());
+        assertEquals(60000, GameManager.getInstance().getPrepareDurationInMs());
+        assertEquals(400, GameManager.getInstance().getTilesPerRegion());
+        assertEquals(480, GameManager.getInstance().getWindowHeight());
+        assertEquals(640, GameManager.getInstance().getWindowWidth());
     }
 }
---
+++ core/src/test/java/com/s31b/castleoffense/game/map/TestTile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java.com.s31b.castleoffense.game.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dennis
+ */
+public class TestTile {
+    
+    @Test
+    public void TestCreateTile(){
+        
+    }
+    
+    @Test
+    public void TestGetX(){
+        
+    }
+    
+    @Test
+    public void TestGetY(){
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
