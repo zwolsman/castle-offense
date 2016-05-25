fe54564cdd572b0a785afeebc1c9e0a055a0b46d
Berlo,Nick N.C.H. van <n.vanberlo@student.fontys.nl>
2016-05-25 15:43:05 +0200

    Added buttons in main menu  - spel maken  - meespelen

--- core/src/com/s31b/castleoffense/ui/MainMenu.java
+++ core/src/com/s31b/castleoffense/ui/MainMenu.java
@@ -7,22 +7,15 @@ package com.s31b.castleoffense.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Cursor;
-import com.badlogic.gdx.graphics.Cursor.SystemCursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.s31b.castleoffense.CastleOffense;
 import com.s31b.castleoffense.game.CoGame;
@@ -36,6 +29,7 @@ public class MainMenu implements Screen {
     private imageButton buttonPlay;
     private imageButton buttonInfo;
     private imageButton buttonQuit;
+    private imageButton buttonJoin;
     private Image background;
     private Cursor customCursor;
     private Skin skin;
@@ -65,6 +59,14 @@ public class MainMenu implements Screen {
                 co.setScreen(new GameMenu(co, game, game.getPlayerById(1))); 
             };
         });
+        buttonJoin = new imageButton(new Texture(Gdx.files.internal("GUIMenu/buttonMainJoin.png")), new Texture(Gdx.files.internal("GUIMenu/buttonMainJoinDown.png")), new Texture(Gdx.files.internal("GUIMenu/buttonMainJoinHover.png")));
+        buttonJoin.addListener( new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
+                
+            };
+        });
         buttonInfo = new imageButton(new Texture(Gdx.files.internal("GUIMenu/buttonMainInfo.png")), new Texture(Gdx.files.internal("GUIMenu/buttonMainInfoDown.png")), new Texture(Gdx.files.internal("GUIMenu/buttonMainInfo.png")));
         buttonInfo.addListener( new ClickListener() {       
             @Override
@@ -87,13 +89,15 @@ public class MainMenu implements Screen {
         width: 200 px
         Height: 60 px
         horizontal pos: half of the screen horizontally
-        Vertical pos: 30, 50, 70 procent of the screen vertically */
+        Vertical pos: 35, 45, 55, 65 procent of the screen vertically */
         buttonPlay.setSize(200, 60);
+        buttonJoin.setSize(200, 60);
         buttonInfo.setSize(200, 60);
         buttonQuit.setSize(200, 60);
-        buttonPlay.setPosition((Gdx.graphics.getWidth() / 2) - (buttonPlay.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 70);
-        buttonInfo.setPosition((Gdx.graphics.getWidth() / 2) - (buttonPlay.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 50);
-        buttonQuit.setPosition((Gdx.graphics.getWidth() / 2) - (buttonPlay.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 30);
+        buttonPlay.setPosition((Gdx.graphics.getWidth() / 2) - (buttonPlay.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 65);
+        buttonJoin.setPosition((Gdx.graphics.getWidth() / 2) - (buttonJoin.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 55);
+        buttonInfo.setPosition((Gdx.graphics.getWidth() / 2) - (buttonInfo.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 45);
+        buttonQuit.setPosition((Gdx.graphics.getWidth() / 2) - (buttonQuit.getWidth() / 2), Gdx.graphics.getHeight() / 100 * 35);
 
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
@@ -107,6 +111,7 @@ public class MainMenu implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.addActor(background);
         stage.addActor(buttonPlay);
+        stage.addActor(buttonJoin);
         stage.addActor(buttonInfo);
         stage.addActor(buttonQuit);
         stage.act();
