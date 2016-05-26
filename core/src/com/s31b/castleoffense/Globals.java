7a7bc32ae3d143ff719bfdbe8b1defa744f2ac60
Zwolsman,Marvin M.W. <m.zwolsman@student.fontys.nl>
2016-05-26 15:42:38 +0200

    Added a global client used for multiplayer purpose

--- core/src/com/s31b/castleoffense/Globals.java
+++ core/src/com/s31b/castleoffense/Globals.java
@@ -5,6 +5,8 @@
  */
 package com.s31b.castleoffense;
 
+import com.s31b.castleoffense.server.KryoClient;
+
 /**
  *
  * @author fhict
@@ -14,7 +16,7 @@ public class Globals {
     public static final Boolean DEBUG = false;
     public static final int TILES_X = 17 * 2, TILES_Y = 12;
     public static final int TILE_WIDTH = 40, TILE_HEIGHT = 40;
-
+    public static KryoClient client;
     //Base Prices
     public static final int PRICE_OFFENSIVE = 5;
     public static final int PRICE_DEFENSIVE = 200;
@@ -33,4 +35,5 @@ public class Globals {
 
     // Castle properties
     public static final int CASTLE_HP = 10;
+
 }
