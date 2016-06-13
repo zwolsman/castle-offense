1341516db3421fefe4a7dd8e81bdb6f227ac92fc
Zwolsman,Marvin M.W. <m.zwolsman@student.fontys.nl>
2016-06-13 12:08:36 +0200

    Created new packets for the lobby

--- core/src/com/s31b/castleoffense/server/packets/CreateGamePacket.java
+++ core/src/com/s31b/castleoffense/server/packets/CreateGamePacket.java
@@ -18,4 +18,8 @@ public class CreateGamePacket implements IPacket {
     public String getName() {
         return this.name;
     }
+
+    public void setName(String n) {
+        this.name = n;
+    }
 }
