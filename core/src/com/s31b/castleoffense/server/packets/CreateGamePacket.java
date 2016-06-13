6aa3c5db518fa225407d2fe92ccede704cbdf52f
Zwolsman,Marvin M.W. <m.zwolsman@student.fontys.nl>
2016-06-13 10:35:43 +0200

    Made it possible to give a name to a game

--- core/src/com/s31b/castleoffense/server/packets/CreateGamePacket.java
+++ core/src/com/s31b/castleoffense/server/packets/CreateGamePacket.java
@@ -6,4 +6,16 @@ package com.s31b.castleoffense.server.packets;
  */
 public class CreateGamePacket implements IPacket {
 
+    public CreateGamePacket() {
+
+    }
+
+    public CreateGamePacket(String name) {
+        this.name = name;
+    }
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
 }
